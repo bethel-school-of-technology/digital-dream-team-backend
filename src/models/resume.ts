e4170098156555d73cd4989e7444ac12d9d30b6a
micahbugbee <micahbugbee@htomail.com>
2023-06-03 07:13:17 -0500

    Added interfaces for data sets and tweaked schema

--- src/models/resume.ts
+++ src/models/resume.ts
@@ -1,66 +1,164 @@
 import { Document, Schema, Model, model } from 'mongoose';
 import { IUser } from './user';
 
+interface Identity {
+    firstName: string;
+    lastName: string;
+    title: string;
+    address: string;
+    phone: string;
+    email: string;
+    linkedIn?: string;
+}
+
+interface Skills {
+    skills?: string[];
+}
+
+interface Jobs {
+    title?: string;
+    company?: string;
+    start?: string;
+    end?: string;
+    accomplishments?: string;
+}
+
+interface Projects {
+    title?: string;
+    start?: string;
+    end?: string;
+    accomplishments?: string;
+}
+
+interface Education {
+    school?: string;
+    degree?: string;
+    dateAcquired?: string;
+}
+
+interface Certifications {
+    certification?: string;
+    provider?: string;
+    dateAcquired?: string;
+}
+
 interface IResume extends Document {
-   firstName: string;
-   lastName: string;
-   title: string;
-   address: string;
-   phone: string; 
-   email: string;
-   skills : string;
-   job: string;
-   project: string; 
-   education: string;
-   certification: string;
+   identity: Identity;
+   skills : Skills[];
+   jobs: Jobs[];
+   project: Projects[];
+   education: Education[];
+   certification: Certifications[];
    userId: IUser['_id'];
 }
 
 const resumeSchema: Schema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    }, 
-    lastName: {
-        type: String,
-        required: true
+    identity: {
+        firstName: {
+            type: String,
+            required: true
+        }, 
+        lastName: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        linkedIn: {
+            type: String,
+            required: false
+        }
     },
-    title: {
+    skills: [{
         type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    skills: {
-        type: String,
-        required: true
-    },
-    job: {
-        type: String,
-        required: true
-    },
-    project: {
-        type: String,
-        required: true
-    },
-    education: {
-        type: String,
-        required: true
-    }, 
-    certification: {
-        type: String,
-        required: true
-    },
+        required: false
+    }],
+    jobs: [{
+        title: {
+            type: String,
+            required: false
+        }, 
+        company: {
+            type: String,
+            required: false
+        },
+        start: {
+            type: String,
+            required: false
+        },
+        end: {
+            type: String,
+            required: false
+        },
+        accomplishments: {
+            type: String,
+            required: false
+        }
+    }],
+    project: [{
+        title: {
+            type: String,
+            required: false
+        }, 
+        company: {
+            type: String,
+            required: false
+        },
+        start: {
+            type: String,
+            required: false
+        },
+        end: {
+            type: String,
+            required: false
+        },
+        accomplishments: {
+            type: String,
+            required: false
+        }
+    }],
+    education: [{
+        school: {
+            type: String,
+            required: false
+        }, 
+        degree: {
+            type: String,
+            required: false
+        },
+        dateAcquired: {
+            type: String,
+            required: false
+        }
+    }], 
+    certification: [{
+        certification: {
+            type: String,
+            required: false
+        }, 
+        provider: {
+            type: String,
+            required: false
+        },
+        dateAcquired: {
+            type: String,
+            required: false
+        }
+    }],
     userId: {
         type: Schema.Types.ObjectId,
         ref: 'User',
